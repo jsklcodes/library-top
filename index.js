3fe6d3dff476b6b89bb0fdd63304a2a421965050
Kallyne Layane <hellookallyne@gmail.com>
2025-01-02 10:50:10 -0300

    Add Book constructor function

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const generateId = (() => {
   return () => id++;
 })();
 
+// === MARK: Book ===
+// Constructor function for books
+function Book(title, author, pages, read) {
+  this.id = generateId();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
 // === MARK: Dialog ===
 // Logic to make the `dialog` element interactive
 const addBookDialog = document.querySelector('#add-book-dialog');
