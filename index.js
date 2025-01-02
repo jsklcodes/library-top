740875c0c0dc33170c9a171e0e9da3b19eafd94f
Kallyne Layane <hellookallyne@gmail.com>
2025-01-02 10:38:08 -0300

    Add logic to make the  element interactive

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello');
+// === MARK: Dialog ===
+// Logic to make the `dialog` element interactive
+const addBookDialog = document.querySelector('#add-book-dialog');
+const openDialogButton = document.querySelector('#open-dialog-button');
+const closeDialogButton = document.querySelector('#close-dialog-button');
+
+const handleOpenDialog = () => addBookDialog.showModal();
+const handleCloseDialog = () => addBookDialog.close();
+
+openDialogButton.addEventListener('click', handleOpenDialog);
+closeDialogButton.addEventListener('click', handleCloseDialog);
